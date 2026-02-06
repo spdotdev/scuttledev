9a0fd3c011751df0237b0cd4e713620b1e463f1b
Stanislav (Stas) Plotnikov <stplotnikov@gmail.com>
2026-02-07 00:08:10 +0100

    Add vCard file reference in Vite configuration for improved file accessibility

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: 'index.html',
+                vcard: 'scuttledev.vcf',
             },
         },
     },
