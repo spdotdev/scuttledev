43ad6a260a7626b588fa7197639547927f4d7a14
Stanislav (Stas) Plotnikov <stplotnikov@gmail.com>
2026-02-07 00:16:20 +0100

    Remove vCard file and its reference from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: 'index.html',
-                vcard: 'scuttledev.vcf',
             },
         },
     },
